2068ccb18db4f1751686c4924214fe687b253871
Eirfat Choudhury <eirfathchoudhury@gmail.com>
2023-03-10 01:04:41 +0000

    Refactored code to use redux on blogs and user data

--- bloglist-frontend/src/reducers/blogReducer.test.js
+++
@@ -1,8 +0,0 @@
-import blogReducer from "./blogReducer";
-import deepFreeze from "deep-freeze";
-
-describe('blogReducer', () => {
-    test('Get all blogs', () => {
-        const state = [{}]
-    })
-})
